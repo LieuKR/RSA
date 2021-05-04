ede7941224fcfbcdbcfd257cb4070d9481f4a0f2
LieuKR <korlieu@gmail.com>
2021-05-04 18:23:42 +0900

    N과 pi값 정의

--- RSA/src/Make_RSA_key.java
+++ RSA/src/Make_RSA_key.java
@@ -24,12 +24,12 @@ public class Make_RSA_key{
     	} else if(isprime(q) == false) {
     		System.out.println("q가 소수가 아닙니다");
     		return;
-    	} 
-    	
-    	// p,q 판별 완료. 이후 키 생성 로직 진행
-
+    	}
     	// 2번 삭제
     	// 3. N = p*q, φ(N)=(p-1)*(q-1)
+    	int N = p*q;
+    	int pi = (p-1)*(q-1);
+    	
     	// 4. φ(N)보다 작고, φ(N)와 서로소인 정수 e를 찾음. 이때 e값에 랜덤값을 이용하여 변동을 줄 것
     	// 5. Extended euclidean algorithm을 이용, de ≡ 1 (mod φ(N))를 만족하는 d를 찾는다.
     	// 6. (N,e)는 공용 키, (N,d)는 개인 키
